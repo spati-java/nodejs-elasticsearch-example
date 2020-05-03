30c860b54fe78a0987e668c1ba4a6ff9fe9f7e27
Sourav Pati <sourav.pati@objectstream.com>
2020-05-03 14:04:27 -0500

    more chnages

--- handler.js
+++ handler.js
@@ -25,9 +25,6 @@ const { body } = await client.search({
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
 
 module.exports.autocomplete = async event => {
