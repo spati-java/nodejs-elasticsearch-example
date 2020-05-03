9a22ced92b9caeff30f6c981d4e9bb6ad3936f6f
Sourav Pati <sourav.pati@objectstream.com>
2020-05-03 13:58:39 -0500

    added auto complete feature

--- handler.js
+++ handler.js
@@ -1,12 +1,25 @@
 'use strict';
 
-module.exports.hello = async event => {
+const { Client } = require('@elastic/elasticsearch')
+const client = new Client({node: 'http://localhost:9200'})
+
+module.exports.findAllProfile = async event => {
+// Let's search!
+const { body } = await client.search({
+  index: 'profile',
+  // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+  body: {
+    query: {
+     "match_all": {}
+    }
+  }
+})
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        profiles: body.hits.hits
       },
       null,
       2
@@ -16,3 +29,32 @@ module.exports.hello = async event => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.autocomplete = async event => {
+  // Let's search!
+const { body } = await client.search({
+  index: 'profile',
+  // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+  body: {
+    query: {
+      "match": {
+        "jobTitle": {
+          "query": event.pathParameters.title, 
+          "operator": "and"
+        }
+      }
+    }
+  }
+})
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        profiles: body.hits.hits
+      },
+      null,
+      2
+    ),
+  };
+}
